34f29e1c44ba585db7ce93ed61c4020c77b75e69
Matheus Mangualde <matheus.oliveira@onecontrol.io>
2025-06-20 11:23:50 -0300

    feat: add function to open mysql connection

--- go.mod
+++ go.mod
@@ -3,11 +3,13 @@ module github.com/relaunch-cot/lib-relaunch-cot
 go 1.24.1
 
 require (
+	github.com/go-sql-driver/mysql v1.9.3
 	google.golang.org/grpc v1.73.0
 	google.golang.org/protobuf v1.36.6
 )
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
 	golang.org/x/net v0.38.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
 	golang.org/x/text v0.23.0 // indirect
---
+++ repositories/mysql/mysql.go
@@ -0,0 +1,35 @@
+package mysql
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Client struct {
+	db *sql.DB
+}
+
+var DB *sql.DB
+
+func InitMySQL(ctx context.Context, user, password, host string, port int, dbname string) error {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=true", user, password, host, port, dbname)
+
+	var err error
+	DB, err = sql.Open("mysql", dsn)
+	if err != nil {
+		return fmt.Errorf("failed to open MySQL connection: %w", err)
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	if err := DB.PingContext(ctx); err != nil {
+		return fmt.Errorf("failed to ping MySQL: %w", err)
+	}
+
+	return nil
+}
